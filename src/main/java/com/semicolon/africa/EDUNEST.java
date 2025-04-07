452db1a9235e70cba52d6e89608b046e00ee0c43
Samson Ibironke <samsonibironke2@gmail.com>
2025-04-07 20:45:43 +0100

    EduNest Project main

---
+++ src/main/java/com/semicolon/africa/EDUNEST.java
@@ -0,0 +1,11 @@
+package com.semicolon.africa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDUNEST {
+    public static void main(String[] args) {
+        SpringApplication.run(EDUNEST.class, args);
+    }
+}
